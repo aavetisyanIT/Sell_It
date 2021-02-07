032f0b31805303bfc99ed3e283cf2150fbbf2199
aavetisyanit <aavetisyanit@gmail.com>
2021-02-07 16:54:54 -0600

    Create ListingEditScreen

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Screen from './app/components/Screen';
 
-import RegisterScreen from './app/screens/RegisterScreen';
+import ListingEditScreen from './app/screens/ListingEditScreen';
 
 export default function App() {
 	return (
 		<Screen>
-			<RegisterScreen />
+			<ListingEditScreen />
 		</Screen>
 	);
 }
