e377bd28b47d72f5d24de92a54749939bbbb0546
aavetisyanit <aavetisyanit@gmail.com>
2021-02-07 10:16:32 -0600

    Refactor ListItem component

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import MessagesScreen from './app/screens/MessagesScreen';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
+import ListItem from './app/components/ListItem';
 
 export default function App() {
-	return <MessagesScreen />;
+	return (
+		<Screen>
+			<ListItem
+				title='My title'
+				ImageComponent={<Icon name='email' />}
+			/>
+		</Screen>
+	);
 }
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -8,6 +8,7 @@ import colors from '../config/colors';
 export default function ListItem({
 	title,
 	subTitle,
+	ImageComponent,
 	image,
 	onPress,
 	renderRightActions,
@@ -19,12 +20,17 @@ export default function ListItem({
 				underlayColor={colors.light}
 			>
 				<View style={styles.container}>
-					<Image style={styles.image} source={image} />
-					<View>
+					{ImageComponent}
+					{image && (
+						<Image style={styles.image} source={image} />
+					)}
+					<View style={styles.detailsContainer}>
 						<AppText style={styles.title}>{title}</AppText>
-						<AppText style={styles.subTitle}>
-							{subTitle}
-						</AppText>
+						{subTitle && (
+							<AppText style={styles.subTitle}>
+								{subTitle}
+							</AppText>
+						)}
 					</View>
 				</View>
 			</TouchableHighlight>
@@ -37,11 +43,14 @@ const styles = StyleSheet.create({
 		flexDirection: 'row',
 		padding: 15,
 	},
+	detailsContainer: {
+		marginLeft: 10,
+		justifyContent: 'center',
+	},
 	image: {
 		width: 70,
 		height: 70,
 		borderRadius: 35,
-		marginRight: 10,
 	},
 	subTitle: {
 		color: colors.medium,
