df16bf402f8311bd16d911f1f23c117d4fb1b8aa
aavetisyanit <aavetisyanit@gmail.com>
2021-02-07 16:17:08 -0600

    Reorganize components dir. Create forms dir

---
+++ app/components/forms/index.js
@@ -0,0 +1,4 @@
+export { default as AppForm } from './AppForm';
+export { default as AppFormFlield } from './AppFormFlield';
+export { default as ErrorMessage } from './ErrorMessage';
+export { default as SubmitButton } from './SubmitButton';
